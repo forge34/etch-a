cfade9f280134188ff05c9b04d77efc171d33b56
mhmddabdeltawab21 <mohamedabdeltawab338@gmail.com>
2023-09-07 01:38:55 +0300

    added input

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 let container = document.querySelector('.container')
+let input = document.querySelector('.grid-input')
+let btn = document.querySelector('.btn')
 
-let grid_size = 100
+let grid_size = input.value
 
-for (let i = 0;i < grid_size;i++){
-    for (let j = 0;j < grid_size;j++)
-    {
-        let cell = document.createElement('div')
-        cell.classList.add('cell')
 
-        container.append(cell)
+function set_grid(g_size){
+    for (let i = 0;i < g_size;i++){
+        for (let j = 0;j < g_size;j++)
+        {
+            let cell = document.createElement('div')
+            cell.classList.add('cell')
+    
+            container.append(cell)
+        }
     }
 }
+
+set_grid(grid_size)
 
 function add_active(cell){
     cell.classList.add('active')
